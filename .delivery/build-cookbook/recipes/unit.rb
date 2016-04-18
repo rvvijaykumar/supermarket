3c63ec9d935a997beb92ed8c7a9364cd5126e2d9
Robb Kidd <rkidd@chef.io>
2016-04-18 15:17:12 -0400

    Add initial version of a unit testing recipe.
    Currently works with delivery-cli running locally on a Mac with the Docker
    for Mac Beta. Needs more around prereqs for Docker.

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -2,4 +2,16 @@
 # Cookbook Name:: build-cookbook
 # Recipe:: unit
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Chef, All Rights Reserved.
+
+include_recipe 'build-cookbook::prepare'
+
+ruby_execute 'run the specs' do
+  command 'bundle exec rake setup:docker db:test:prepare spec'
+  cwd node['components']['supermarket']['src']
+  version node['components']['supermarket']['ruby_version']
+  environment ({
+    'POSTGRES_IP' => 'docker.local',
+    'REDIS_URL' => 'redis://docker.local:6379/0/supermarket'
+  })
+end
