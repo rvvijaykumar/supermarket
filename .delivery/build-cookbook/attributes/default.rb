658eb662771b72bead5ec45612ece2784c499889
Robb Kidd <rkidd@chef.io>
2016-04-18 12:33:01 -0400

    Correct the name of .ruby-version

--- .delivery/build-cookbook/attributes/default.rb
+++ .delivery/build-cookbook/attributes/default.rb
@@ -1,4 +1,4 @@
 default['components']['supermarket']['src'] = File.join(node['delivery_builder']['repo'], 'src/supermarket')
 
-supermarket_version_file = File.join(node['delivery_builder']['repo'], 'src/supermarket/.ruby_version')
+supermarket_version_file = File.join(node['delivery_builder']['repo'], 'src/supermarket/.ruby-version')
 default['components']['supermarket']['ruby_version'] = IO.read(supermarket_version_file).strip
